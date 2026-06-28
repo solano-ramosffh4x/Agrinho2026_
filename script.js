fa9cab8aaeff9cee6828bd0ba3778a519f415d21
solano-ramosffh4x <solano.ramos@escola.pr.gov.br>
2026-06-28 12:29:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let tamanhoFonte = 16;
 
+function entrar(){
+
+    let nome = document.getElementById("nome").value;
+
+    document.getElementById("mensagem").innerHTML =
+    "Olá, " + nome + "!";
+}
+
+function aumentarFonte(){
+
+    tamanhoFonte += 2;
+
+    document.body.style.fontSize = tamanhoFonte + "px";
+
+}
+
+function diminuirFonte(){
+
+    if(tamanhoFonte > 12){
+
+        tamanhoFonte -= 2;
+
+        document.body.style.fontSize = tamanhoFonte + "px";
+
+    }
+
+}
